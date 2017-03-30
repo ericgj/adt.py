525629101537c1790f25d016b3692ffadc75e58a
Eric Gjertsen <ericgj72@gmail.com>
2017-03-29 23:18:49 -0400

    version bump (0.0.6)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = "adt.py",
-  version = "0.0.5",
+  version = "0.0.6",
   author = "Eric Gjertsen",
   author_email = "ericgj72@gmail.com",
   description = (
